8576839415819f6d23ab4cee2a4e8bd60243b217
Dmytro Shumsky <schooldima1@gmail.com>
2024-01-02 15:23:28 +0200

    fix linter 3

--- src/main/java/com/example/AppUser.java
+++ src/main/java/com/example/AppUser.java
@@ -7,22 +7,24 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 import java.time.LocalDate;
 import java.time.Period;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table
-@Setter @Getter
+@Setter
+@Getter
 public class AppUser {
     @Id
     @GeneratedValue
     private int id;
     private String name;
     private String email;
+
     @Transient
     private int age;
     private LocalDate date;
-
 
     public int getAge() {
         return Period.between(date, LocalDate.now()).getYears();
--- src/main/java/com/example/AppUserRepository.java
+++ src/main/java/com/example/AppUserRepository.java
@@ -2,9 +2,10 @@ package com.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
-    Optional<AppUser>findUserByEmail(String email);
+    Optional<AppUser> findUserByEmail(String email);
 }
